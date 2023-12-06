2685f0e5ff139c0ee5e8c97a599ae1e28b8b17d3
SelimGedik <153108626+SelimGedik@users.noreply.github.com>
2023-12-06 20:06:01 +0300

    Odev
    Pc'den alınan rasgele sayıyı , kullanıcıdan sayı alarak tahmin eden metot.

---
+++ Random Sayi Karşilaştirma/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Sayi_Karşilaştirma
+{  
+    public class Tahmin
+    {       
+        public int RandomSayi()
+        {
+            int x = new Random().Next(1,11);
+
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            while (y != x)
+            {                               
+                Console.WriteLine(y + " yanlış tahmin ");
+                y = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if(y == x)
+            {
+                Console.WriteLine(y + " doğru tahmin " + x);
+            }              
+            return 0;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Tahmin deneme = new Tahmin();
+            deneme.RandomSayi();
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ Random Sayi Karşilaştirma/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Random Sayi Karşilaştirma")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Random Sayi Karşilaştirma")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("344e9206-af9b-490a-a85e-f0944d73cf1e")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
